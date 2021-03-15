60e2b0e082025d3559140d747529e099c8f484e4
Steve <zqc111123@gmail.com>
2021-03-15 23:46:40 +0800

    Removed node_modules

--- helloworld.js
+++ helloworld.js
@@ -1,9 +1,26 @@
-var http = require('http');
-var fs = require('fs');
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
 http.createServer(function (req, res) {
-    fs.readFile('./html_files/index.html', function(err, data) {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
+    let q = url.parse(req.url,true);
+    let filename = "./html_files"+q.pathname;
+    if(filename === "./html_files/") {
+        filename = "./html_files/index.html"
+    }
+    console.log(filename);
+    fs.readFile(filename, function(err, data) {
+        if(err) {
+            fs.readFile("./html_files/404.html",function (err,data) {
+                res.writeHead(404,{'Content-Type': 'text/html'});
+                res.write(data);
+                return res.end();
+            });
+        }
+        else {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            return res.end();
+        }
+
     });
 }).listen(8080);
